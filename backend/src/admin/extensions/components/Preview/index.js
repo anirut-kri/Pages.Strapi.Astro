8f1d6ed9820b516893abe18dd2500a6e2bf6b069
anirut-kri <anirut.kri@ascendcorp.com>
2024-03-04 12:33:01 +0700

    2nd commit

--- backend/src/admin/extensions/components/Preview/index.js
+++ backend/src/admin/extensions/components/Preview/index.js
@@ -7,7 +7,14 @@ const Preview = () => {
         <Button
             variant="secondary"
             startIcon={<Eye />}
-            onClick={() => alert("Hello World")}
+            onClick={() => {
+                fetch(
+                    "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/ec332889-b526-4d8e-89a9-b20d5ab787a1",
+                    {
+                        method: "POST",
+                    }
+                );
+            }}
         >
             Preview
         </Button>
--- backend/src/admin/extensions/components/Publish/index.js
+++ backend/src/admin/extensions/components/Publish/index.js
@@ -7,7 +7,14 @@ const Publish = () => {
         <Button
             variant="secondary"
             startIcon={<ArrowUp />}
-            onClick={() => alert("Hello World")}
+            onClick={() => {
+                fetch(
+                    "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/21f31d59-88c1-4226-91bd-4844ed048282",
+                    {
+                        method: "POST",
+                    }
+                );
+            }}
         >
             Publish
         </Button>
